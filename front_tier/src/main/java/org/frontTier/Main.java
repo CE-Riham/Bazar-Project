5d505d4084a09307492b69c7f314c8048ad2f3b4
Amjad Kayed <amjad.kayed@hotmail.com>
2024-03-23 02:01:32 +0200

    test: test commit

--- front_tier/src/main/java/org/frontTier/Main.java
+++ front_tier/src/main/java/org/frontTier/Main.java
@@ -2,6 +2,6 @@ package main.java.org.frontTier;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello front tier");
+        System.out.println("Hello front tier :)");
     }
 }
