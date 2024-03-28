e34df5e952e492ab289909343fd88f2b5b2df6d2
RihamKatout <54861182+RihamKatout@users.noreply.github.com>
2024-03-28 21:37:00 +0200

    update: add sendPost request to HttpRequestSender

--- common/src/main/java/org/common/utils/HttpRequestSender.java
+++ common/src/main/java/org/common/utils/HttpRequestSender.java
@@ -2,7 +2,6 @@ package org.common.utils;
 
 import com.google.gson.Gson;
 import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
@@ -12,8 +11,25 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.common.enums.StatusResponse;
 
+/**
+ * This class provides utility methods for sending HTTP requests.
+ * It includes methods for sending GET and POST requests.
+ */
 public class HttpRequestSender {
 
+    /**
+     * Private constructor to prevent instantiation of this utility class.
+     */
+    private HttpRequestSender() {
+        throw new IllegalStateException("HttpRequestSender class");
+    }
+
+    /**
+     * Sends a GET request to the specified URL and returns the response body as a string.
+     * If an exception occurs during the request, it returns a JSON string with an error message.
+     * @param url The URL to send the GET request to.
+     * @return The response body as a string.
+     */
     public static String sendGetRequest(String url) {
         try (CloseableHttpClient client = HttpClients.createDefault()) {
             HttpGet request = new HttpGet(url);
@@ -26,14 +42,22 @@ public class HttpRequestSender {
         }
     }
 
+    /**
+     * Sends a POST request to the specified URL with the provided JSON string as the request body.
+     * Returns the response body as a string.
+     * If an exception occurs during the request, it returns a JSON string with an error message.
+     * @param url The URL to send the POST request to.
+     * @param json The JSON string to include in the request body.
+     * @return The response body as a string.
+     */
     public static String sendPostRequest(String url, String json) {
-        try (CloseableHttpClient client = HttpClients.createDefault()){
+        try (CloseableHttpClient client = HttpClients.createDefault()) {
             HttpPost request = new HttpPost(url);
             HttpEntity stringEntity = new StringEntity(json, ContentType.APPLICATION_JSON);
             request.setEntity(stringEntity);
             return client.execute(request, httpResponse -> EntityUtils.toString(httpResponse.getEntity()));
 
-        }catch (Exception e) {
+        } catch (Exception e) {
             ApiResponse apiResponse = new ApiResponse(StatusResponse.ERROR);
             apiResponse.setMessage(e.getMessage());
             return new Gson().toJson(apiResponse);
